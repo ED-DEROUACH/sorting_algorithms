e416a879e6921bb64a5238dabba6570d30ad8cee
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-09-26 01:07:27 +0100

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -4,7 +4,7 @@
 
 /**
  * counting_sort - Sorts an array of integers in ascending order using
- * the Counting sort algorithm.
+ *                 the Counting sort algorithm.
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
@@ -14,46 +14,46 @@ void counting_sort(int *array, size_t size)
 	int *count = NULL, *output = NULL;
 	size_t i;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    for (i = 1; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
 
-    count = malloc(sizeof(int) * (max + 1));
-    if (count == NULL)
-        return;
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+		return;
 
-    output = malloc(sizeof(int) * size);
-    if (output == NULL)
-    {
-        free(count);
-        return;
-    }
+	output = malloc(sizeof(int) * size);
+	if (output == NULL)
+	{
+		free(count);
+		return;
+	}
 
-    for (i = 0; i <= (size_t)max; i++)
-        count[i] = 0;
+	for (i = 0; i <= (size_t)max; i++)
+		count[i] = 0;
 
-    for (i = 0; i < size; i++)
-        count[array[i]]++;
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
 
-    for (i = 1; i <= (size_t)max; i++)
-        count[i] += count[i - 1];
+	for (i = 1; i <= (size_t)max; i++)
+		count[i] += count[i - 1];
 
-    print_array(count, max + 1);
+	print_array(count, max + 1);
 
-    for (i = 0; i < size; i++)
-    {
-        output[count[array[i]] - 1] = array[i];
-        count[array[i]]--;
-    }
+	for (i = 0; i < size; i++)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
 
-    for (i = 0; i < size; i++)
-        array[i] = output[i];
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-    free(count);
-    free(output);
+	free(count);
+	free(output);
 }
