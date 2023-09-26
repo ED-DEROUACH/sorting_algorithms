f68b2282f49cfed3270fd7ba4b6bc1d51cd737ba
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-09-26 01:09:44 +0100

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,59 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
+#include "stdlib.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order using
- *                 the Counting sort algorithm.
- * @array: The array to be sorted.
- * @size: The size of the array.
+ * counting_sort - sorts an array of integers in ascending order using the
+ * Counting sort algorithm
+ * @array: array to sort
+ * @size: size of the array to sort
+ *
+ * Return: void
  */
 void counting_sort(int *array, size_t size)
 {
-	int max = array[0];
-	int *count = NULL, *output = NULL;
-	size_t i;
+	int i, max;
+	int *count = NULL, *copy = NULL;
+	size_t j, temp, total = 0;
 
-	if (size < 2)
+
+	if (array == NULL || size < 2)
 		return;
-
-	for (i = 1; i < size; i++)
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
+		return;
+	for (j = 0, max = 0; j < size; j++)
 	{
-		if (array[i] > max)
-			max = array[i];
+		copy[j] = array[j];
+		if (array[j] > max)
+			max = array[j];
 	}
-
 	count = malloc(sizeof(int) * (max + 1));
 	if (count == NULL)
-		return;
-
-	output = malloc(sizeof(int) * size);
-	if (output == NULL)
 	{
-		free(count);
+		free(copy);
 		return;
 	}
-
-	for (i = 0; i <= (size_t)max; i++)
+	for (i = 0; i <= max; i++)
 		count[i] = 0;
-
-	for (i = 0; i < size; i++)
-		count[array[i]]++;
-
-	for (i = 1; i <= (size_t)max; i++)
-		count[i] += count[i - 1];
-
-	print_array(count, max + 1);
-
-	for (i = 0; i < size; i++)
+	for (j = 0; j < size; j++)
+		count[array[j]] += 1;
+	for (i = 0; i <= max; i++)
 	{
-		output[count[array[i]] - 1] = array[i];
-		count[array[i]]--;
+		temp = count[i];
+		count[i] = total;
+		total += temp;
 	}
-
-	for (i = 0; i < size; i++)
-		array[i] = output[i];
-
+	for (j = 0; j < size; j++)
+	{
+		array[count[copy[j]]] = copy[j];
+		count[copy[j]] += 1;
+	}
+	print_array(count, max + 1);
 	free(count);
-	free(output);
+	free(copy);
 }
