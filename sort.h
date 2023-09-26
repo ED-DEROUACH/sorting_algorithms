3fe1f819f498a9984ef446ecdf183c5c5b40b594
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-09-25 17:27:17 -0700

    up

--- sort.h
+++ sort.h
@@ -31,5 +31,8 @@ void swap_nodes(listint_t *node1, listint_t *node2, listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
