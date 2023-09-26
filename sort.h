29b95577444c1aa0699f5cfa6723fa2e907dc0ef
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-09-25 17:19:51 -0700

    up

--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ void cocktail_sort_list(listint_t **list);
 void swap_nodes(listint_t *node1, listint_t *node2, listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
 #endif
